3dca5cf33b703ec478395e936f2ab29a482f88e9
Gunnar Abrahamsson <per.gunnar.abrahamsson@gmail.com>
2023-12-01 13:22:14 +0100

    formatting

--- src/2023/day1.ts
+++ src/2023/day1.ts
@@ -4,47 +4,54 @@ const inputs = fs
 	.readFileSync(path.resolve(__dirname + "/inputs/1.txt"), "utf-8")
 	.split("\n");
 
-const getValue = (input: string) => {
-	const array = input.split("");
-	const first = array.findIndex((value) => +value);
-	const last = array.findLastIndex((value) => +value);
+const star1 = () => {
+    const getValue = (input: string) => {
+        const array = input.split("");
+        const first = array.findIndex((value) => +value);
+        const last = array.findLastIndex((value) => +value);
+    
+        return +(array[first] + array[last]);
+    };
+    console.log(
+        inputs.reduce((acc, curr) => {
+            return acc + getValue(curr);
+        }, 0)
+    );
+}
 
-	return +(array[first] + array[last]);
-};
-console.log(
-	inputs.reduce((acc, curr) => {
-		return acc + getValue(curr);
-	}, 0)
-);
+star1();
 
-const numbers = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
-const getValue2 = (input: string) => {
-    let string = ""
-    const foundNumbers: number[] = [];
-    for(let i = 0; i < input.length; i++) {
-        if(+input[i]) {
-            foundNumbers.push(+input[i])
-            string = ""
-            continue;
+const star2 = () => {
+    const numbers = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+    const getValue2 = (input: string) => {
+        let string = ""
+        const foundNumbers: number[] = [];
+        for(let i = 0; i < input.length; i++) {
+            if(+input[i]) {
+                foundNumbers.push(+input[i])
+                string = ""
+                continue;
+            }
+            string = string + input[i];
+            const foundNumber = numbers.find((num) => string.match(RegExp(`${num}`)))
+            
+            if(foundNumber) {
+                foundNumbers.push(numbers.indexOf(foundNumber) + 1)
+                string = "";
+                i = i - foundNumber.length + 1
+            }
         }
-        string = string + input[i];
-        const foundNumber = numbers.find((num) => string.match(RegExp(`${num}`)))
-        
-        if(foundNumber) {
-            foundNumbers.push(numbers.indexOf(foundNumber) + 1)
-            string = "";
-            i = i - foundNumber.length + 1
-        }
-    }
-	const first = foundNumbers[0];
-	const last = foundNumbers[foundNumbers.length - 1];
-    return +`${first}${last}`
-};
+        const first = foundNumbers[0];
+        const last = foundNumbers[foundNumbers.length - 1];
+        return +`${first}${last}`
+    };
+    
+    console.log(
+        inputs.reduce((acc, curr) => {
+            return acc + getValue2(curr);
+        }, 0)
+    );
+}
 
-console.log(
-	inputs.reduce((acc, curr) => {
-		return acc + getValue2(curr);
-	}, 0)
-);
-
+star2();
 

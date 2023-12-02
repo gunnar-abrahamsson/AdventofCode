4b9807a30025d550fa9ee3d816ee062cbc5c1c9f
Gunnar Abrahamsson <per.gunnar.abrahamsson@gmail.com>
2023-12-02 11:05:39 +0100

    day 2

--- src/2023/day2.ts
+++ src/2023/day2.ts
@@ -1,3 +1,66 @@
 import fs from 'fs'
 import path from 'path'
 const inputs = fs.readFileSync(path.resolve(__dirname + '/inputs/2.txt'), 'utf-8').split('\n')
+
+const star1 = () => {
+    const max = {
+        red: 12,
+        green: 13,
+        blue: 14
+    }
+    let total = 0;
+    const isPossible = (draw: string[]) => {
+        const isInPossible = draw.some((balls) => {
+            const [number, color] = balls.trim().split(' ') as [number, 'red' | 'green' | 'blue'];
+            return max[color] < number
+
+        })
+
+        return !isInPossible
+    }
+    inputs.forEach((value) => {
+        const [game, balls] = value.split(':');
+        const draws = balls.split(';').map(draw => draw.trim())
+        const isInPossibleGame = draws.some(draw => {
+            const balls = draw.split(',')
+            return !isPossible(balls)
+        })
+        const [,gameNr] = game.trim().split(' ')
+        if(!isInPossibleGame) {
+            total = total + +gameNr;
+        }
+    })
+
+    return total;
+
+}
+
+console.log(star1())
+
+const star2 = () => {
+    let total = 0;
+    inputs.forEach((value) => {
+        const maxColor = {
+            red: 0,
+            blue: 0,
+            green: 0
+        }
+        const [game, balls] = value.split(':');
+        const draws = balls.split(';').map(draw => draw.trim())
+        draws.forEach(draw => {
+            draw.split(',').forEach(value => {
+                const [number, color] = value.trim().split(' ') as [string, 'red' | 'green' | 'blue'];
+                if(maxColor[color] < +number) {
+                    maxColor[color] = +number
+                }
+            })
+            
+        })
+        total = total + (maxColor['red'] * maxColor['blue'] * maxColor['green'])
+    })
+
+    return total;
+
+}
+
+console.log(star2())
